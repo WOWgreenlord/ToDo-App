654ba8394afbe8653db49047151b0dcae7cc51a9
Миша Дьяков <dock.misha@gmail.com>
2025-03-25 16:37:58 +0300

    Добавление и удаление заданий

--- js/main.js
+++ js/main.js
@@ -2,22 +2,55 @@ let buttons = {
   remove: document.querySelector(".container__button_remove_task"),
   add: document.querySelector(".container__button_add_task"),
   done: document.querySelector(".container__button_done_task"),
+  modalAdd: document.querySelector(".modal__content_btn"),
 };
-let checkboxes = document.querySelectorAll(".task__container_checkbox");
+let addTaskModal = {
+  name: document.querySelector(".modal__content_name"),
+  description: document.querySelector(".modal__content_description"),
+};
+// let checkboxes = document.querySelectorAll(".task__container_checkbox");
+let container = document.querySelector(".container");
+let checkboxes = container.getElementsByClassName("task__container_checkbox");
 let checkbox = document.querySelector(".task__container_checkbox");
 let modal = document.querySelector(".container__modal");
-
+let date = new Date();
+let year = date.getUTCFullYear();
+let month = date.getUTCMonth() + 1;
+let day = date.getUTCDate();
+let hours = date.getHours();
+let mins = date.getMinutes();
 buttons.remove.addEventListener("click", removeTask);
 function removeTask() {
-  for (checkbox of checkboxes) {
-    if (checkbox.checked) {
-      checkbox.parentElement.classList.add("removeTask");
-      let delTasks = document.querySelectorAll(".removeTask");
-      for (task of delTasks) {
-        task.remove();
-      }
-    }
+  // Создаем массив из отмеченных задач (чтобы не работать с "живой" коллекцией)
+  const tasksToRemove = Array.from(checkboxes)
+    .filter((checkbox) => checkbox.checked)
+    .map((checkbox) => checkbox.parentElement);
+  // Удаляем все отмеченные задачи
+  tasksToRemove.forEach((task) => task.remove());
+}
+buttons.modalAdd.addEventListener("click", addTask);
+function addTask() {
+  if (addTaskModal.name.value == 0 || addTaskModal.description.value == 0) {
+    alert("Type name and description of task");
+    return;
   }
+  let label = document.createElement("label");
+  label.classList.add("task__container");
+  label.innerHTML = `<h2 class="task__container_name">${
+    addTaskModal.name.value
+  }</h2><p class="task__container_description">${
+    addTaskModal.description.value
+  }</p><p class="task__container_date_create">${hours}:${
+    mins < 10 ? (mins = "0" + mins) : mins
+  } ${day}.${
+    month <= 10 ? (month = "0" + month) : month
+  }.${year}</p><input type="checkbox" class="task__container_checkbox"/>`;
+  container.append(label);
+  addTaskModal.name.value = null;
+  addTaskModal.description.value = null;
+  console.log(checkboxes);
+  console.log(checkboxes.length);
+  closeModal();
 }
 function openModal() {
   modal.style.display = "block";
@@ -25,5 +58,3 @@ function openModal() {
 function closeModal() {
   modal.style.display = "none";
 }
-// let modal = document.createElement('div');
-// modal.innerHTML = '<h1></h1>';
